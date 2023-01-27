4c387f4e3cb7477b8f16101df467f90842d0ad4d
AlzyWelzy <welzyalzy@gmail.com>
2023-01-27 19:12:05 +0530

    working

---
+++ Chapter 2/2-7.py
@@ -0,0 +1,7 @@
+name = " Manvendra Rajpoot "
+_name = " \t\nM\t\na\t\nn\t\nv\t\ne\t\nn\t\nd\t\nr\t\na\t\n \t\nR\t\na\t\nj\t\np\t\no\t\no\t\nt\t\n "
+print(name.strip())
+print(name.lstrip())
+print(name.rstrip())
+
+print(_name)
