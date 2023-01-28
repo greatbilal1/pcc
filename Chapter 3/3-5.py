2aedcaa5b82597387ea03f970ae2ae01fdddc1a3
AlzyWelzy <welzyalzy@gmail.com>
2023-01-28 12:54:42 +0530

    done 3-5

---
+++ Chapter 3/3-5.py
@@ -0,0 +1,9 @@
+guests = ["Akash", "Rajdeep", "Rajveer"]
+print(f"Come to dinner, {guests[0]}.")
+print(f"Come to dinner, {guests[1]}.")
+print(f"Come to dinner, {guests[2]}.")
+print(f"{guests[0]} cannot come as he is going out with his girlfriend.")
+guests[0] = "Abhishek"
+print(f"Come to dinner, {guests[0]}.")
+print(f"Come to dinner, {guests[1]}.")
+print(f"Come to dinner, {guests[2]}.")
