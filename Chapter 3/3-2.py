8ed418f2b5fd44351e13ebdddfe85e9e750ca76f
AlzyWelzy <welzyalzy@gmail.com>
2023-01-28 09:42:51 +0530

    completed 3-2

---
+++ Chapter 3/3-2.py
@@ -0,0 +1,5 @@
+names = ["Tarun Ghawri", "Vishal Sahu", "Farhan Qazi", "Arfat Khan"]
+print(f"Hello There, My Dear Friend {names[0]}.")
+print(f"Hello There, My Dear Friend {names[1]}.")
+print(f"Hello There, My Dear Friend {names[2]}.")
+print(f"Hello There, My Dear Friend {names[3]}.")
