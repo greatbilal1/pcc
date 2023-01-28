21be1fe9b6fd9c2bfbace88e30af4d7a153c75de
AlzyWelzy <welzyalzy@gmail.com>
2023-01-28 09:47:15 +0530

    completed 3-3

---
+++ Chapter 3/3-3.py
@@ -0,0 +1,6 @@
+car = ["Toyota", "Tesla"]
+motorcycle = ["Harley Davidson", "Honda"]
+print(f"I would like to own a {car[0]} car.")
+print(f"I would like to own a {car[1]} car.")
+print(f"I would like to own a {motorcycle[0]} motorcycle.")
+print(f"I would like to own a {motorcycle[1]} motorcycle.")
