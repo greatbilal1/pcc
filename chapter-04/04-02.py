1cfae817127955ad9cd570a5d078f693e64d8d5b
AlzyWelzy <welzyalzy@gmail.com>
2023-01-29 22:19:07 +0530

    04-02 donee

---
+++ chapter-04/04-02.py
@@ -0,0 +1,7 @@
+animals = ["dog", "cat", "hamster"]
+
+for animal in animals:
+    print(animal)
+    print(f"A {animal} would make a great pet animal.")
+
+print("Any of these animals would make a great pet!")
