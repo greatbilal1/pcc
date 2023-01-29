2deeccf35c0d26182a48492c6790dde35baa2c15
AlzyWelzy <welzyalzy@gmail.com>
2023-01-29 22:17:15 +0530

    I changed content where it should bbe

---
+++ chapter-04/04-01.py
@@ -0,0 +1,7 @@
+pizzas = ["Paneer", "Mushroom", "Chicken"]
+
+for pizza in pizzas:
+    print(pizza)
+    print(f"I like {pizza}.")
+
+print("I LOVE PIZZA!")
--- chapter-04/04-02.py
+++
@@ -1,7 +0,0 @@
-pizzas = ["Paneer", "Mushroom", "Chicken"]
-
-for pizza in pizzas:
-    print(pizza)
-    print(f"I like {pizza}.")
-
-print("I LOVE PIZZA!")
