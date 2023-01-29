a9708fc102f27434c80e8dd13d340c454f6ede29
AlzyWelzy <welzyalzy@gmail.com>
2023-01-29 19:20:14 +0530

    3-8 done

---
+++ Chapter 3/3-8.py
@@ -0,0 +1,19 @@
+locations = ["tokyo", "osaka", "nainital", "kyoto", "manali"]
+print(locations)
+
+print(sorted(locations))
+print(locations)
+
+print(sorted(locations, reverse=True))
+print(locations)
+
+locations.reverse()
+print(locations)
+locations.reverse()
+print(locations)
+
+locations.sort()
+print(locations)
+
+locations.sort(reverse=True)
+print(locations)
