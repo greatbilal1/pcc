2f08e87c188339e552b6c3d6b0a074e0a095a44e
AlzyWelzy <welzyalzy@gmail.com>
2023-02-01 20:06:48 +0530

    reworked

--- chapter-05/05-07.py
+++ chapter-05/05-07.py
@@ -2,8 +2,18 @@ favorite_fruits = ["apple", "banana", "mango"]
 
 playerFruit = None
 
-while playerFruit != "n":
+# while playerFruit != "n":
+#     playerFruit = input("Enter a fruit name to check or type n to exit: ")
+#     if playerFruit in favorite_fruits:
+#         print(f"You really like {playerFruit}")
+#     else:
+#         print(f"You don't really like {playerFruit}")
+
+while True:
     playerFruit = input("Enter a fruit name to check or type n to exit: ")
+    if playerFruit == "n":
+        exit()
+
     if playerFruit in favorite_fruits:
         print(f"You really like {playerFruit}")
     else:
