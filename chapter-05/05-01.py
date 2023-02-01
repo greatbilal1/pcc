e1a160f29ce6a79341c1b1e2a6e1c33376d342f9
AlzyWelzy <welzyalzy@gmail.com>
2023-02-01 15:27:22 +0530

    Working

--- chapter-05/05-01.py
+++ chapter-05/05-01.py
@@ -4,3 +4,10 @@ print("AlzyWelzy" == car)
 
 print(f"Is the car name, WelzyAlzy? I predict False")
 print("WelzyAlzy" == car)
+
+car = "Audi"
+print(f"Is the car name, Audi? I predict True")
+print("Audi" == car)
+
+print(f"Is the car name, Cadi? I predict False")
+print("Cadi" == car)
