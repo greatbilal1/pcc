23603bb303953fcb3b67d79fe663c8ba3de6ef9f
AlzyWelzy <welzyalzy@gmail.com>
2023-02-01 11:33:00 +0530

    working

--- chapter-04/04-13.py
+++ chapter-04/04-13.py
@@ -8,3 +8,17 @@ buffet = (
 
 for shabji in buffet:
     print(shabji)
+
+# won't work
+# buffet[1]="chole khulche"
+
+buffet = (
+    "chole khulche",
+    "paneer toofani",
+    "malai kofta",
+    "mushroom paneer",
+    "khadhai paneer",
+)
+
+for shabji in buffet:
+    print(shabji)
