b60b4c6cee6f04d5af3b223a941c578658627f00
AlzyWelzy <welzyalzy@gmail.com>
2023-02-01 15:26:41 +0530

    05-01 done

---
+++ chapter-05/05-01.py
@@ -0,0 +1,6 @@
+car = "AlzyWelzy"
+print(f"Is the car name, AlzyWelzy? I predict True")
+print("AlzyWelzy" == car)
+
+print(f"Is the car name, WelzyAlzy? I predict False")
+print("WelzyAlzy" == car)
