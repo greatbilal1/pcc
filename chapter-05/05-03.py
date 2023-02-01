3e3ad9457cd721884b218cb4c32fbd62d3607bc7
AlzyWelzy <welzyalzy@gmail.com>
2023-02-01 18:54:19 +0530

    05-03 done

---
+++ chapter-05/05-03.py
@@ -0,0 +1,18 @@
+## Part 1
+alien_color = ["green", "yellow", "red"]
+
+player_name = input("Enter your username: ")
+player_color = input("Guess a color from yellow, green, orange or red: ")
+
+if player_color in alien_color:
+    print("You have guess correct color of alien, that was green.")
+
+## Part 2
+
+if "green" in alien_color:
+    print("You have guess correct color of alien, that was green.")
+
+## Part 3
+
+if "green" in alien_color:
+    print("")
