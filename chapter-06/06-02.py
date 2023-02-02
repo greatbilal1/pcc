a8a2901bdb60ca40ee513de9b066e891dcce624a
AlzyWelzy <welzyalzy@gmail.com>
2023-02-02 21:17:50 +0530

    06-02 done

---
+++ chapter-06/06-02.py
@@ -0,0 +1,11 @@
+alzy = {"name": "alzy", "favNum": 1}
+welzy = {"name": "welzy", "favNum": 2}
+manvendra = {"name": "manvendra", "favNum": 3}
+rajpoot = {"name": "rajpoot", "favNum": 4}
+alzywelzy = {"name": "alzywelzy", "favNum": 5}
+
+print(f"{alzy.get('name')} favorite number is {alzy.get('favNum')}")
+print(f"{welzy.get('name')} favorite number is {welzy.get('favNum')}")
+print(f"{manvendra.get('name')} favorite number is {manvendra.get('favNum')}")
+print(f"{rajpoot.get('name')} favorite number is {rajpoot.get('favNum')}")
+print(f"{alzywelzy.get('name')} favorite number is {alzywelzy.get('favNum')}")
