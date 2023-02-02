54378684ca3369195428ab6db4a6635b29f5f745
AlzyWelzy <welzyalzy@gmail.com>
2023-02-02 21:25:31 +0530

    06-03 done

---
+++ chapter-06/06-03.py
@@ -0,0 +1,13 @@
+keywords = {
+    "variable": "A variable is a container that holds some value",
+    "constant": "It's a special type of variable whose value can't be changed.",
+    "is": "It a keywords which check if both the conditions points to same location",
+    "in": "It's a keyword which check if a condition(variable) is located in a list or tuple",
+    "list": "It's a list",
+}
+
+print(keywords.get("variable"))
+print(keywords.get("constant"))
+print(keywords.get("is"))
+print(keywords.get("in"))
+print(keywords.get("list"))
