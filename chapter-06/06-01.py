a5c9e492a03bc89974c71430004aa892c6c1f4c7
AlzyWelzy <welzyalzy@gmail.com>
2023-02-02 21:11:51 +0530

    06-01 done

---
+++ chapter-06/06-01.py
@@ -0,0 +1,6 @@
+person = {"firstName": "manvendra", "lastName": "rajpoot", "age": 20, "city": "jhansi"}
+
+print(person.get("firstName", "NOT AVAILABLE!"))
+print(person.get("lastName", "NOT AVAILABLE!"))
+print(person.get("age", "NOT AVAILABLE!"))
+print(person.get("city", "NOT AVAILABLE!"))
