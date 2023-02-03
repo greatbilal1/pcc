0b122ce4e44feabb8e0245af4b2753efb4b5b9f1
AlzyWelzy <welzyalzy@gmail.com>
2023-02-03 16:03:37 +0530

    06-04 done

---
+++ chapter-06/code_2.py
@@ -0,0 +1,12 @@
+user_0 = {
+    "username": "efermi",
+    "first": "enrico",
+    "last": "fermi",
+    "dob": "14-07-2002",
+    "birthPlace": "jhansi",
+    "nationality": "indian",
+}
+
+for key, value in user_0.items():
+    print(key)
+    print(value)
