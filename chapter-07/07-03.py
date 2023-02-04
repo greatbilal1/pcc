056effcddadc9fb87a1b623259466c3f55f4cb4b
AlzyWelzy <welzyalzy@gmail.com>
2023-02-04 11:32:47 +0530

    07-03 done

---
+++ chapter-07/07-03.py
@@ -0,0 +1,6 @@
+num = int(input("Enter a number and I will check if it's a multiple of 10: "))
+
+if num % 10 == 0:
+    print("It's a multiple of 10.")
+else:
+    print("It's not a multiple of 10.")
