3a5aa373cf7f64a5d70d9a2c85d1851c6479872b
AlzyWelzy <welzyalzy@gmail.com>
2023-02-04 20:48:18 +0530

    08-01 done

---
+++ chapter-08/08-01.py
@@ -0,0 +1,7 @@
+def display_message():
+    print(
+        "The first line uses the keyword def to inform Python that you’re defining a function. This is the function definition, which tells Python the name of the function and, if applicable, what kind of information the function needs to do its job. The parentheses hold that information."
+    )
+
+
+display_message()
