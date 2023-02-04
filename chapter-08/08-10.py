89e8877ac4573695373b8db3e3181ae91b304c2c
AlzyWelzy <welzyalzy@gmail.com>
2023-02-05 00:25:53 +0530

    0810 done

---
+++ chapter-08/08-10.py
@@ -0,0 +1,28 @@
+showMessages = [
+    "ohayo gozaimasu",
+    "arigato senpai",
+    "onii chan i love you",
+    "good morning world",
+    "i love python",
+]
+
+sendMessages = []
+
+
+def show_messages(messages, workList):
+
+    for message in messages:
+        print(message)
+        workList.append(message)
+        showMessages.remove(message)
+
+
+show_messages(showMessages[:], sendMessages)
+
+
+def sent_messages(workList):
+    for work in workList:
+        print(work)
+
+
+sent_messages(sendMessages[:])
