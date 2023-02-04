0828f71a777abe0ff05e9fb523847ca4533fc94d
AlzyWelzy <welzyalzy@gmail.com>
2023-02-04 22:20:05 +0530

    08-05 done

---
+++ chapter-08/08-05.py
@@ -0,0 +1,8 @@
+def describe_city(city="jhansi", country="india"):
+    print(f"{city.title()} is in {country.title()}")
+
+
+describe_city()
+describe_city("tokyo", "japan")
+describe_city(country="usa", city="sillicon valley")
+describe_city(country="india", city="new delhi")
