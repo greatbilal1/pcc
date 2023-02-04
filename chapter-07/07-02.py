5b7236a3df6d1527d4ac888692234a169f77c26d
AlzyWelzy <welzyalzy@gmail.com>
2023-02-04 11:29:20 +0530

    07-02

---
+++ chapter-07/07-02.py
@@ -0,0 +1,6 @@
+numofpeople = int(input("How many people are their in dinner group: "))
+
+if numofpeople > 8:
+    print("They will have to wait for a table.")
+else:
+    print("Their table is ready.")
