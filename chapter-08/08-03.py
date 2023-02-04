3a6f41bb0bdd2400767d2776d6735b3bc52931c5
AlzyWelzy <welzyalzy@gmail.com>
2023-02-04 22:14:48 +0530

    08-03 done

---
+++ chapter-08/08-03.py
@@ -0,0 +1,7 @@
+def make_shirt(size, message):
+    print(
+        f"The size of the t-shirt is {size}cm and the message printed on it is this -> {message}"
+    )
+
+
+make_shirt(message="EHE TE NANDAYO", size="21")
