97106553905130a08474bcc829ed4719d1e285c8
AlzyWelzy <welzyalzy@gmail.com>
2023-02-04 12:27:32 +0530

    doing loops while

---
+++ chapter-07/counting.py
@@ -0,0 +1,16 @@
+current_number = 0
+
+while current_number < 10:
+    current_number += 1
+
+    # This will print even number
+    # if current_number % 2 == 0:
+    #     print(current_number)
+
+    # continue
+
+    # This will print odd number
+    # if current_number % 2 == 0:
+    #     continue
+
+    # print(current_number)
