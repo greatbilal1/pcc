ea7c050776e1ad3705e0bc6cbbe277ac8eb260c2
AlzyWelzy <welzyalzy@gmail.com>
2023-02-04 22:17:34 +0530

    08-04

--- chapter-08/08-04.py
+++ chapter-08/08-04.py
@@ -1,7 +1,9 @@
-def make_shirt(size, message):
+def make_shirt(size=100, message="I love Python"):
     print(
         f"The size of the t-shirt is {size}cm and the message printed on it is this -> {message}"
     )
 
 
+make_shirt()
+make_shirt(90, "BRUH")
 make_shirt(message="EHE TE NANDAYO", size=21)
