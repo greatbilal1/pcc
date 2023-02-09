0d91dc0b1cb113ae08819bce2cafba8317f4660a
Manvendra Rajpoot <96976159+AlzyWelzy@users.noreply.github.com>
2023-02-09 11:23:31 +0530

    Update 10-04.py

--- chapter-10/10-04.py
+++ chapter-10/10-04.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-#  = input("What's your name? ")
+# name = input("What's your name? ")
 path = Path("guests.txt").write_text(
     f"""It's name is {input(f"What's your name? ")}."""
 )
