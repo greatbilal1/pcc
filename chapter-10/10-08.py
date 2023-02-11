6a9bd4961f0fce3b365f78c5a92ad7fa7c5b8ccb
AlzyWelzy <welzyalzy@gmail.com>
2023-02-11 13:43:18 +0530

    working

---
+++ chapter-10/10-08.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+catPath = Path("cats.txt")
+dogPath = Path("dogs.txt")
+
+
+try:
+    catContent = catPath.read_text()
+    dogContent = dogPath.read_text()
+except FileNotFoundError as e:
+    print(e)
+else:
+    print(catContent)
+    print(dogContent)
