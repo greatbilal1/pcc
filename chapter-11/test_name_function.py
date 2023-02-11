0faac0aaed4729659e8b5ee800615c3d55cfd9c5
AlzyWelzy <welzyalzy@gmail.com>
2023-02-11 20:31:13 +0530

    working

--- chapter-11/test_name_function.py
+++ chapter-11/test_name_function.py
@@ -5,3 +5,10 @@ def test_first_last_name():
     """Do names like 'Janis Joplin' work?"""
     formatted_name = get_formatted_name("Janis", "Joplin")
     assert formatted_name == "Janis Joplin"
+
+
+def test_first_last_middle_name():
+    """Do names like 'Janis Joplin' work?"""
+    # formatted_name = get_formatted_name("Janis", "Joplin")
+    formatted_name = get_formatted_name("Manvendra", "Rajpoot", "Singh")
+    assert formatted_name == "Manvendra Singh Rajpoot"
