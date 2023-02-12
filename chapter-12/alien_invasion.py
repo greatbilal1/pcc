29db03b86bb0b7002691fd8535b8ddd0926058ae
AlzyWelzy <welzyalzy@gmail.com>
2023-02-12 21:50:48 +0530

    about to do exercises

--- chapter-12/alien_invasion.py
+++ chapter-12/alien_invasion.py
@@ -32,18 +32,25 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-
-            # Redraw the screen during each pass through the loop.
-            # self.screen.fill(self.bg_color)
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
+            self._check_events()
+            self._update_screen()
             self.clock.tick(60)
+
+    def _update_screen(self):
+        # Redraw the screen during each pass through the loop.
+        # self.screen.fill(self.bg_color)
+        """Update images on the screen, and flip to the new screen."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
+
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
 
 if __name__ == "__main__":
