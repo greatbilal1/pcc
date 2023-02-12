20e6b483ba3f729ead70c32c0a6870428de96540
AlzyWelzy <welzyalzy@gmail.com>
2023-02-12 21:25:04 +0530

    making the app

--- chapter-12/ship.py
+++ chapter-12/ship.py
@@ -10,8 +10,11 @@ class Ship:
         self.screen_rect = ai_game.screen.get_rect()
 
         # Load the ship image and get its rect.
-        self.image = pygame.image.load("images/ship.bmp")
+        self.image = pygame.image.load("./images/ship.bmp")
         self.rect = self.image.get_rect()
+
+        # Start each new ship at the bottom of the screen
+        self.rect.midbottom = self.screen_rect.midbottom
 
     def blitme(self):
         """Draw the ship at its current location."""
