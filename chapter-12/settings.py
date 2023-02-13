e85ffbe0c669dd40cf78a36bcd92d4b0ce0365ef
AlzyWelzy <welzyalzy@gmail.com>
2023-02-13 22:57:47 +0530

    .

--- chapter-12/settings.py
+++ chapter-12/settings.py
@@ -10,3 +10,6 @@ class Settings:
         # self.screen_height = 1080
         self.bg_color = (230, 230, 230)
         # self.bg_color = (0, 0, 255)
+
+        # Ship settings
+        self.ship_speed = 1.5
