1932a1abc7c3f9fd9c34a19e1c8c2e84a52b39c8
AlzyWelzy <welzyalzy@gmail.com>
2023-02-13 19:34:01 +0530

    working

--- Code/pcc_3e-main/chapter_12/refactoring/alien_invasion.py
+++ Code/pcc_3e-main/chapter_12/refactoring/alien_invasion.py
@@ -37,10 +37,7 @@ class AlienInvasion:
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
                     # Move the ship to the right.
-                    self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    # Move the ship to the left.
-                    self.ship.rect.y += 1
+                    self.ship.rect.x += 1
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
