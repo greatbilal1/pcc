bc1e640e245e78ff43cb384808cf28167d5e3913
AlzyWelzy <welzyalzy@gmail.com>
2023-03-01 19:02:55 +0530

    finally fixed the graph

--- chapter-15/scatter_squares.py
+++ chapter-15/scatter_squares.py
@@ -1,12 +1,15 @@
 import matplotlib.pyplot as plt
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
+# x_values = [1, 2, 3, 4, 5]
+x_values = range(1, 1001)
+# y_values = [1, 4, 9, 16, 25]
+y_values = [x**2 for x in x_values]
 
 plt.style.use("seaborn")
 fig, ax = plt.subplots()
 # ax.scatter(2, 4, s=200)
-ax.scatter(x_values, y_values, s=200)
+# ax.scatter(x_values, y_values, s=200)
+ax.scatter(x_values, y_values, s=10)
 
 
 # Set chart title and label axes.
@@ -16,5 +19,8 @@ ax.set_ylabel("Square of Value", fontsize=14)
 
 # Set size of tick labels.
 ax.tick_params(labelsize=14)
+
+# Set the range for each axis.
+ax.axis([0, 1100, 0, 1_100_000])
 
 plt.show()
