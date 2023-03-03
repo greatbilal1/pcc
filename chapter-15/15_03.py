849f58ba868dba3842372f0982eb45ed039bc966
AlzyWelzy <welzyalzy@gmail.com>
2023-03-03 21:54:05 +0530

    Fixed value names

--- chapter-15/15_03.py
+++ chapter-15/15_03.py
@@ -27,8 +27,8 @@ while True:
     ax.plot(rw.x_values, rw.y_values, linewidth=1)
 
     ax.set_title("15-03", fontsize=24)
-    ax.set_xlabel("Value", fontsize=14)
-    ax.set_ylabel("Square of Value", fontsize=14)
+    ax.set_xlabel("X Values", fontsize=14)
+    ax.set_ylabel("Y Values", fontsize=14)
 
     # ax.set_aspect("equal")
 
