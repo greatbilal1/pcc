033b8eebdc99e9c2d0d8350f4a12ab638d1a3fa7
AlzyWelzy <welzyalzy@gmail.com>
2023-03-03 21:53:08 +0530

    finally fixed all of them

--- chapter-15/15_04.py
+++ chapter-15/15_04.py
@@ -26,7 +26,7 @@ while True:
     print(rw.y_values)
     ax.plot(rw.x_values, rw.y_values, linewidth=1)
 
-    ax.set_title("15-03", fontsize=24)
+    ax.set_title("15-04", fontsize=24)
     ax.set_xlabel("Value", fontsize=14)
     ax.set_ylabel("Square of Value", fontsize=14)
 
