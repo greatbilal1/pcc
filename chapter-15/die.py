1ef0ed46516bcc43528cd7c4a69afa369732a952
AlzyWelzy <welzyalzy@gmail.com>
2023-03-05 11:49:21 +0530

    installed pip and black

--- chapter-15/die.py
+++ chapter-15/die.py
@@ -1,6 +1,8 @@
 from random import randint
 
+
 class Die:
     """A class representing a single die."""
 
-    def __init__(self,num_sides=6):
+    def __init__(self, num_sides=6):
+        print("HEHE")
