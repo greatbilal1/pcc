b6bcab4ba45f8e04a67fcae1f615749cac287ffd
AlzyWelzy <welzyalzy@gmail.com>
2023-03-05 12:17:15 +0530

    created a die class successfully

--- chapter-15/die.py
+++ chapter-15/die.py
@@ -5,4 +5,9 @@ class Die:
     """A class representing a single die."""
 
     def __init__(self, num_sides=6):
-        print("HEHE")
+        """Assume a six-sided die."""
+        self.num_sides = num_sides
+
+    def roll(self):
+        """Return a random value between 1 and number of sides."""
+        return randint(1, self.num_sides)
