1589aa59d88c49479d6cfed350309879b164fcec
AlzyWelzy <welzyalzy@gmail.com>
2023-03-06 11:40:58 +0530

    fixed it

--- chapter-15/15_07.py
+++ chapter-15/15_07.py
@@ -2,15 +2,16 @@ import plotly.express as px
 
 from die import Die
 
-# Create Two D8 Dice
-die_1 = Die(8)
-die_2 = Die(8)
+# Create Three D6 Dice
+die_1 = Die()
+die_2 = Die()
+die_3 = Die()
 
 
 # Make some rolls, and store results in a list.
 results = []
-for roll_num in range(1000):
-    result = die_1.roll() + die_2.roll()
+for roll_num in range(100_000):
+    result = die_1.roll() + die_2.roll() + die_3.roll()
     results.append(result)
 
 print(results)
@@ -18,8 +19,8 @@ print(results)
 
 # Analyze the results.
 frequencies = []
-max_result = die_1.num_sides + die_2.num_sides
-poss_results = range(2, max_result + 1)
+max_result = die_1.num_sides + die_2.num_sides + die_3.num_sides
+poss_results = range(3, max_result + 1)
 for value in poss_results:
     frequency = results.count(value)
     frequencies.append(frequency)
@@ -27,7 +28,7 @@ for value in poss_results:
 print(frequencies)
 
 # Visualize the results.
-title = "Results of Rolling Two D8 Dice 1,000 Times"
+title = "Results of Rolling Three D6 Dice 100,000 Times"
 labels = {"x": "Result", "y": "Frequency of Result"}
 fig = px.bar(x=poss_results, y=frequencies, title=title, labels=labels)
 
@@ -35,4 +36,4 @@ fig = px.bar(x=poss_results, y=frequencies, title=title, labels=labels)
 fig.update_layout(xaxis_dtick=1)
 
 # fig.show()
-fig.write_html("dice_visual_two_d8.html")
+fig.write_html("dice_visual_three_d6.html")
