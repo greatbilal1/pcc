3d91ed55dc91e38cb0ab2b07922311849ed6e666
AlzyWelzy <welzyalzy@gmail.com>
2023-03-06 12:34:02 +0530

    15_08 improved

--- chapter-15/15_08.py
+++ chapter-15/15_08.py
@@ -3,31 +3,18 @@ import plotly.express as px
 from die import Die
 
 # Create a D6 and a D10.
-die_1 = Die()
+die_1 = Die(6)
 die_2 = Die(10)
 
 
 # Make some rolls, and store results in a list.
-# results = []
-# for roll_num in range(50_000):
-#     result = die_1.roll() + die_2.roll()
-#     results.append(result)
-
-# print(results)
-
 results = [die_1.roll() + die_2.roll() for _ in range(50_000)]
 print(results)
 
 
 # Analyze the results.
-# frequencies = []
 max_result = die_1.num_sides + die_2.num_sides
 poss_results = range(2, max_result + 1)
-# for value in poss_results:
-#     frequency = results.count(value)
-#     frequencies.append(frequency)
-
-# print(frequencies)
 
 frequencies = [results.count(_) for _ in poss_results]
 print(frequencies)
