215785a4fb266a8a499a4128bd20bdf091ef2955
AlzyWelzy <welzyalzy@gmail.com>
2023-03-09 13:43:11 +0530

    1603

--- chapter-16/16_03.py
+++ chapter-16/16_03.py
@@ -5,41 +5,37 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 
 # path = Path("./weather_data/sitka_weather_2021_simple.csv")
-# path = Path("./weather_data/death_valley_2021_simple.csv")
-path = Path("./weather_data/sitka_weather_2021_full.csv")
+path = Path("./weather_data/death_valley_2021_simple.csv")
 lines = path.read_text().splitlines()
 
 reader = csv.reader(lines)
 header_row = next(reader)
 
-# print(lines)
 
 # Extract dates and high temperatures.
-dates, total_rain = [], []
+dates, highs, lows = [], [], []
 for row in reader:
     current_date = datetime.strptime(row[2], "%Y-%m-%d")
-
     try:
-        print(row)
-        rain = float(row[5])
+        high = int(row[3])
+        low = int(row[4])
     except ValueError:
         print(f"Missing data for {current_date}.")
     else:
         dates.append(current_date)
-        total_rain.append(rain)
+        highs.append(high)
+        lows.append(low)
 
 
 # Plot the high and low temperatures.
 plt.style.use("seaborn")
 fig, ax = plt.subplots()
-ax.plot(dates, total_rain, color="red", alpha=0.5)
-# ax.plot(dates, lows, color="blue", alpha=0.5)
-# ax.fill_between(dates, highs, lows, facecolor="blue", alpha=0.1)
+ax.plot(dates, highs, color="red", alpha=0.5)
+ax.plot(dates, lows, color="blue", alpha=0.5)
+ax.fill_between(dates, highs, lows, facecolor="blue", alpha=0.1)
 
 # Format plot.
-# ax.set_title("Daily High Temperatures, July 2021", fontsize=24)
-# title = "Daily High and Low Temperatures, 2021\nDeath Valley, CA"
-title = "Sitka Rainfall"
+title = header_row[0]
 ax.set_title(title, fontsize=20)
 ax.set_xlabel("", fontsize=16)
 fig.autofmt_xdate()
@@ -47,3 +43,6 @@ ax.set_ylabel("Temperature (F)", fontsize=16)
 ax.tick_params(labelsize=16)
 
 plt.show()
+
+for index, column_header in enumerate(header_row):
+    print(index, column_header)
