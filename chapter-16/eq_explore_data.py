69c3a8550ec2921bb9ec70faf5cf0b52ad71f6b1
AlzyWelzy <welzyalzy@gmail.com>
2023-03-09 22:03:43 +0530

    building a world map

--- chapter-16/eq_explore_data.py
+++ chapter-16/eq_explore_data.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import json
 
+import plotly.express as px
+
 # Read data as a string and convert to a Python object.
 path = Path("./eq_data/eq_data_1_day_m1.geojson")
 contents = path.read_text()
@@ -28,10 +30,13 @@ for eq_dict in all_eq_dicts:
     lats.append(lat)
 
 
-print(mags[:10])
-print(lons[:5])
-print(lats[:5])
+# print(mags[:10])
+# print(lons[:5])
+# print(lats[:5])
 
+title = "Global Earthquakes"
+fig = px.scatter_geo(lat=lats, lon=lons, title=title)
+fig.show()
 
 # Create a more readable version of the data file.
 path = Path("./eq_data/readable_eq_data.geojson")
