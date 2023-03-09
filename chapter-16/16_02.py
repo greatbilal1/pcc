43ddde90dfb0cc792b77721bf5bfae9717519724
AlzyWelzy <welzyalzy@gmail.com>
2023-03-09 11:57:29 +0530

    16_02

--- chapter-16/16_02.py
+++ chapter-16/16_02.py
@@ -6,17 +6,21 @@ import matplotlib.pyplot as plt
 
 # path = Path("./weather_data/sitka_weather_2021_simple.csv")
 # path = Path("./weather_data/death_valley_2021_simple.csv")
-path = Path("./weather_data/sitka_weather_2021_full.csv")
-lines = path.read_text().splitlines()
+path_sitka = Path("./weather_data/sitka_weather_2021_full.csv")
+lines_sitka = path_sitka.read_text().splitlines()
 
-reader = csv.reader(lines)
-header_row = next(reader)
+reader_sitka = csv.reader(lines_sitka)
+header_row_sitka = next(reader_sitka)
 
-# print(lines)
+path_valley = Path("./weather_data/death_valley_2021_full.csv")
+lines_valley = path_valley.read_text().splitlines()
+
+reader_valley = csv.reader(lines_valley)
+header_row_valley = next(reader_valley)
 
 # Extract dates and high temperatures.
-dates, total_rain = [], []
-for row in reader:
+dates, total_rain_sitka = [], []
+for row in reader_sitka:
     current_date = datetime.strptime(row[2], "%Y-%m-%d")
 
     try:
@@ -26,20 +30,33 @@ for row in reader:
         print(f"Missing data for {current_date}.")
     else:
         dates.append(current_date)
-        total_rain.append(rain)
+        total_rain_sitka.append(rain)
 
+dates, total_rain_valley = [], []
+for row in reader_valley:
+    current_date = datetime.strptime(row[2], "%Y-%m-%d")
+
+    try:
+        print(row)
+        rain = float(row[3])
+    except ValueError:
+        print(f"Missing data for {current_date}.")
+    else:
+        dates.append(current_date)
+        total_rain_valley.append(rain)
 
 # Plot the high and low temperatures.
 plt.style.use("seaborn")
 fig, ax = plt.subplots()
-ax.plot(dates, total_rain, color="red", alpha=0.5)
+ax.plot(dates, total_rain_sitka, color="red", alpha=0.5)
+ax.plot(dates, total_rain_valley, color="blue", alpha=0.5)
 # ax.plot(dates, lows, color="blue", alpha=0.5)
-# ax.fill_between(dates, highs, lows, facecolor="blue", alpha=0.1)
+ax.fill_between(dates, total_rain_sitka, total_rain_valley, facecolor="blue", alpha=0.1)
 
 # Format plot.
 # ax.set_title("Daily High Temperatures, July 2021", fontsize=24)
 # title = "Daily High and Low Temperatures, 2021\nDeath Valley, CA"
-title = "Sitka Rainfall"
+title = "Sitka and Death Valley Rainfall"
 ax.set_title(title, fontsize=20)
 ax.set_xlabel("", fontsize=16)
 fig.autofmt_xdate()
