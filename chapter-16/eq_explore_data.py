fe81122b8771a1062f0880d77a3cd9d0d485901a
AlzyWelzy <welzyalzy@gmail.com>
2023-03-09 20:08:55 +0530

    making a list of all earthquakes

--- chapter-16/eq_explore_data.py
+++ chapter-16/eq_explore_data.py
@@ -6,6 +6,10 @@ path = Path("./eq_data/eq_data_1_day_m1.geojson")
 contents = path.read_text()
 all_eq_data = json.loads(contents)
 
+# Examine all earthquakes in the dataset.
+all_eq_dicts = all_eq_data["features"]
+print(len(all_eq_dicts))
+
 # Create a more readable version of the data file.
 path = Path("./eq_data/readable_eq_data.geojson")
 readable_contents = json.dumps(all_eq_data, indent=4)
