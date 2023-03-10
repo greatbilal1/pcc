07b250d144a16115632e7b43673204f31f4ffa83
AlzyWelzy <welzyalzy@gmail.com>
2023-03-10 10:06:16 +0530

    fixed 1000 to 10000

--- chapter-17/python_repos.py
+++ chapter-17/python_repos.py
@@ -2,7 +2,7 @@ import requests
 
 # Make an API Call and check the response.
 url = "https://api.github.com/search/repositories"
-url += "?q=language:python+sort:stars+stars:>1000"
+url += "?q=language:python+sort:stars+stars:>10000"
 
 headers = {"Accept": "application/vnd.github.v3+json"}
 r = requests.get(url, headers=headers)
